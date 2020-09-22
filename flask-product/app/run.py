7aa380d12d48837683c8f13640dc179abbbd5d14
ymdevx3 <y.mori.707@gmail.com>
2020-09-23 02:52:25 +0900

    Add flask product-env on docker

---
+++ flask-product/app/run.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello Flask!"
+
+@app.route("/hello/<name>")
+def hello_someone(name):
+    return "Hello " + name + " !"
+
+if __name__ == "__main__":
+    app.run()
